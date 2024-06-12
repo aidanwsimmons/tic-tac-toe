b177ce1226cc529d8e9c2d55ef40a88f1d132385
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-06-12 13:34:04 -0700

    added gameBoard functionality to print board state, mark cell, and reset

--- app.js
+++ app.js
@@ -1,86 +1,120 @@
+// function GameBoard() {
+//     const rows = 3
+//     const cols = 3
+//     const board = []
+
+//     for(let i = 0; i < rows; i++){
+//         board[i] = []
+//         for(let j = 0; j < cols; j++){
+//             board[i].push(Cell())
+//         }
+//     }
+
+//     const getBoard = () => board
+
+//     const markSquare = (row, col, player) => {
+
+//     }
+
+//     const printBoard = () => {
+//         conso
+//     }
+
+//     return { getBoard, markSquare, printBoard}
+// }
+
+// function Cell() {
+//     let value = 0
+
+//     const addToken = (player) => {
+//         value = player
+//     }
+
+//     const getValue = () => value
+
+//     return {
+//         addToken,
+//         getValue
+//     }
+// }
+
+// function GameController(
+//     playerOneName = "Player One",
+//     playerTwoName = "Player Two"
+// ) {
+//     const board = GameBoard()
+
+//     const players = [
+//         {
+//             name: playerOneName,
+//             token: 'X'
+//         },
+//         {
+//             name: playerTwoName,
+//             token: 'O'
+//         }
+//     ]
+
+//     let activePlayer = players[0]
+
+//     const switchPlayerTurn = () => {
+//         activePlayer = activePlayer === players[0] ? players[1] : players[2]
+//     }
+//     const getActivePlayer = () => activePlayer
+
+//     const printNewRound = () => {
+//         board.printBoard()
+//         console.log(`${getActivePlayer().name}'s turn`)
+//     }
+
+//     const playRound = (row, col) => {
+//         board.markSquare(row, col, getActivePlayer().token)
+
+//         //logic for win/loss/tie
+
+//         switchPlayerTurn()
+//         printNewRound()
+//     }
+
+//     printNewRound()
+
+//     return {
+//         playRound,
+//         getActivePlayer,
+//         getBoard: board.getBoard
+//     }
+// }
+
+
 function GameBoard() {
-    const rows = 3
-    const cols = 3
-    const board = []
-
-    for(let i = 0; i < rows; i++){
-        board[i] = []
-        for(let j = 0; j < cols; j++){
-            board[i].push(Cell())
-        }
-    }
-
-    const getBoard = () => board
-
-    const markSquare = (row, col, player) => {
-
-    }
-
+    const board = [["", "", ""],
+                   ["", "", ""],
+                   ["", "", ""]]
+    
+    //logs board state to console
     const printBoard = () => {
-
+        console.log(board.map(row => row.join(" | ")).join("\n---------\n"))
     }
 
-    return { getBoard, markSquare, printBoard}
-}
-
-function Cell() {
-    let value = 0
-
-    const addToken = (player) => {
-        value = player
-    }
-
-    const getValue = () => value
-
-    return {
-        addToken,
-        getValue
-    }
-}
-
-function GameController(
-    playerOneName = "Player One",
-    playerTwoName = "Player Two"
-) {
-    const board = GameBoard()
-
-    const players = [
-        {
-            name: playerOneName,
-            token: 'X'
-        },
-        {
-            name: playerTwoName,
-            token: 'O'
+    //if cell is unmarked, marks game board cell with player's token
+    const markCell = (row, col, token) => {
+        if(board[row - 1][col - 1] == ""){
+            board[row - 1][col - 1] = token
+            printBoard()
         }
-    ]
-
-    let activePlayer = players[0]
-
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[2]
-    }
-    const getActivePlayer = () => activePlayer
-
-    const printNewRound = () => {
-        board.printBoard()
-        console.log(`${getActivePlayer().name}'s turn`)
+        else{
+            console.log('Cell is already occupied, please choose another')
+        }
     }
 
-    const playRound = (row, col) => {
-        board.markSquare(row, col, getActivePlayer().token)
-
-        //logic for win/loss/tie
-
-        switchPlayerTurn()
-        printNewRound()
+    //returns board to original state
+    const resetBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                board[i][j] = "";
+            }
+        }
     }
 
-    printNewRound()
-
-    return {
-        playRound,
-        getActivePlayer,
-        getBoard: board.getBoard
-    }
+    return {printBoard, markCell, resetBoard}
 }
